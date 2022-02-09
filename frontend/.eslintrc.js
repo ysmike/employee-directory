3908854e5d1f29fd45b697b189658b048a2472f1
ysmike <xcalibur220@gmail.com>
2022-02-09 13:26:38 -0500

    initialize frontend via `create-next-app`
    remove unnecessary files

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-plusplus': 0,
+    'no-param-reassign': 0,
+    'no-unused-vars': 0,
+    'no-irregular-whitespace': 0,
+    'consistent-return': 0,
+    'func-names': 0,
+    'no-restricted-syntax': 0,
+    'no-console': 0,
+    'max-len': 0,
+    'space-before-function-paren': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/prop-types': 0,
+    'import/prefer-default-export': 0,
+    'react/react-in-jsx-scope': 0,
+  },
+};
