b25c25010c662b5f05dc9215d2f5447c3fe1061a
ysmike <xcalibur220@gmail.com>
2022-02-10 11:30:17 -0500

    add a button that opens a modal to create employee

---
+++ frontend/components/CreateModal.js
@@ -0,0 +1,211 @@
+/* eslint-disable import/no-cycle */
+import { useMutation } from '@apollo/client';
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+  Text,
+  useColorModeValue,
+  useDisclosure,
+} from '@chakra-ui/react';
+import gql from 'graphql-tag';
+import { useRef, useState } from 'react';
+import useForm from '../lib/useForm';
+import { ALL_EMPLOYEES_QUERY } from './Employees';
+
+const CREATE_EMPLOYEE_MUTATION = gql`
+  mutation CREATE_EMPLOYEE_MUTATION(
+    # which variables are passed in and what types are they?
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $cell: String
+    $age: Int
+    $gender: String
+    $city: String
+    $state: String
+    $country: String
+    $photoURL: String
+    $countryCode: String
+  ) {
+  createEmployee(
+    data:{
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      cell: $cell
+      age: $age
+      gender: $gender
+      city: $city
+      state: $state
+      country: $country
+      photoURL: $photoURL
+      countryCode: $countryCode
+  }) {
+    id
+    firstName
+    lastName
+    email
+  }
+}
+`;
+
+export default function CreateModal() {
+  const initialRef = useRef();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { inputs, handleChange, resetForm } = useForm({
+    firstName: '',
+    lastName: '',
+    email: '',
+    cell: '',
+    age: 0,
+    gender: '',
+    city: '',
+    state: '',
+    country: '',
+    photoURL: '',
+  });
+
+  const [networkError, setNetworkError] = useState(false);
+  const [createEmployee, { data }] = useMutation(CREATE_EMPLOYEE_MUTATION, {
+    variables: inputs,
+    refetchQueries: [{ query: ALL_EMPLOYEES_QUERY }],
+  });
+
+  const closeModal = () => {
+    resetForm();
+    onClose();
+  };
+
+  const handleSubmit = async (_event) => {
+    // save the input fields in the backend db
+    try {
+      const res = await createEmployee();
+      closeModal();
+    } catch (e) {
+      setNetworkError(e);
+    }
+  };
+
+  const isRequiredFilledIn = inputs.firstName && inputs.lastName && inputs.email;
+  return (
+    <>
+      <Button
+        onClick={onOpen}
+        bg={useColorModeValue('green.200', 'gray.200')}
+        m={5}
+      >
+        Create Employee
+      </Button>
+
+      <Modal
+        initialFocusRef={initialRef}
+        closeOnOverlayClick
+        isOpen={isOpen}
+        onClose={closeModal}
+        size="xl"
+        scrollBehavior="inside"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create an employee</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            {/* optional error message */}
+            {networkError
+            && (
+            <Text
+              color="red"
+              mb={4}
+              p={2}
+              border="1px solid red"
+            >
+              {networkError.message}
+            </Text>
+            )}
+            {/* first & last name */}
+            <Stack direction="row">
+              <FormControl isRequired>
+                <FormLabel>First name</FormLabel>
+                <Input
+                  ref={initialRef}
+                  placeholder="First name"
+                  onChange={handleChange}
+                  name="firstName"
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Last name</FormLabel>
+                <Input
+                  placeholder="Last name"
+                  onChange={handleChange}
+                  name="lastName"
+                />
+              </FormControl>
+            </Stack>
+            {/* email */}
+            <FormControl isRequired mt={4}>
+              <FormLabel>Email</FormLabel>
+              <Input
+                placeholder="Email"
+                onChange={handleChange}
+                name="email"
+              />
+            </FormControl>
+            {/* mobile, age & gender */}
+            <Stack direction="row" mt={4}>
+              <FormControl>
+                <FormLabel>Mobile #</FormLabel>
+                <Input placeholder="Mobile #" onChange={handleChange} name="cell" />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Age</FormLabel>
+                <Input placeholder="Age" onChange={handleChange} name="age" type="number" />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Gender</FormLabel>
+                <Input placeholder="Gender" onChange={handleChange} name="gender" />
+              </FormControl>
+            </Stack>
+            {/* city, state & country */}
+            <Stack direction="row" mt={4}>
+              <FormControl>
+                <FormLabel>City</FormLabel>
+                <Input placeholder="City" onChange={handleChange} name="city" />
+              </FormControl>
+              <FormControl>
+                <FormLabel>State</FormLabel>
+                <Input placeholder="State" onChange={handleChange} name="state" />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Country</FormLabel>
+                <Input placeholder="Country" onChange={handleChange} name="country" />
+              </FormControl>
+            </Stack>
+            {/* photoURL */}
+            <FormControl mt={4}>
+              <FormLabel>Photo URL</FormLabel>
+              <Input placeholder="Photo URL" onChange={handleChange} name="photoURL" />
+            </FormControl>
+          </ModalBody>
+          {/* save & cancel buttons */}
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleSubmit} disabled={!isRequiredFilledIn}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
---
+++ frontend/lib/useForm.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+export default function useForm(initial = {}) {
+  // create a state object for our inputs
+  const [inputs, setInputs] = useState(initial);
+  const initialValues = Object.values(initial).join('');
+
+  // this effect runs when any of the inputs' values change
+  useEffect(() => {
+    setInputs(initial);
+  }, [initialValues]);
+
+  // copy existing state and overwrite the value of the specified key
+  function handleChange({ target: { name, type, value } }) {
+    // HTML automatically converts number to text so we coerce back to number
+    if (type === 'number') value = +value;
+    setInputs({ ...inputs, [name]: value });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key]) => [key, '']),
+    );
+    setInputs(blankState);
+  }
+
+  // return the things we want to surface from this custom hook
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
+}
