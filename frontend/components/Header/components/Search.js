537323c6342d2739930bb799a9dfee0ea5edbb00
ysmike <xcalibur220@gmail.com>
2022-02-10 23:19:13 -0500

    implement search box

--- frontend/components/Header/components/Search.js
+++ frontend/components/Header/components/Search.js
@@ -7,22 +7,21 @@ import { useSearch } from '../../../lib/searchState';
 
 export default function Search() {
   const { search, setSearch, clearSearch } = useSearch();
-  console.log({ search });
   const onChange = (e) => {
     setSearch(e.target.value);
   };
 
   return (
-    <InputGroup w={{ base: '10rem', md: '30rem' }} flex="flex-end">
+    <InputGroup w={{ base: '10rem', md: '20rem', lg: '30rem' }} flex="flex-end">
       <InputLeftElement
         pointerEvents="none"
         children={<SearchIcon color="gray.300" />}
       />
       <Input placeholder="Search" value={search} onChange={onChange} />
-      <Box _onHover={{ cursor: 'pointer' }}>
+      <Box>
         <InputRightElement
           onClick={clearSearch}
-          children={<SmallCloseIcon color="green.500" />}
+          children={<SmallCloseIcon color="cyan.500" _hover={{ cursor: 'pointer' }} />}
         />
       </Box>
     </InputGroup>
